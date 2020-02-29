0ebacc0f702029e8073e3172b9bf959cbda6e522
AliCemilcan <ciftarsl@sheridancollege.ca>
2020-02-29 17:50:13 -0500

    Hello guys

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+ 
+  //title = 'Aero-Trace';
+  movies = [{ title: 'test' }];
+  
+  title;
+  desc;
+  year;
 
   ngOnInit() {
   }
-
+  constructor(private api: ApiService) {
+    this.getMovies();
+  }
+  getMovies = () => {
+    this.api.getAllMovies().subscribe(
+      data => {
+        this.movies = data;
+       
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  movieClicked = (movie) => {
+    console.log(movie.id);
+    this.api.getOneMovie(movie.id).subscribe(
+      data => {
+        this.title = data.title;
+        this.desc = data.desc;
+        this.year = data.year;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
