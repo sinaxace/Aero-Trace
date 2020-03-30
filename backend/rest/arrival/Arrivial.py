c0b8ef6d241ecda0d9ac1d72dc5872788b7def11
hunghanchen <chenhung@sheridancollege.ca>
2020-03-29 20:36:32 -0400

    commit

--- backend/rest/arrival/Arrivial.py
+++ backend/rest/arrival/Arrivial.py
@@ -1,11 +1,13 @@
 import requests
 import json
+import os
 
 
 
 # r = requests.get("https://gtaa-fl-prod.azureedge.net/api/flights/list?type=ARR&day=today&useScheduleTimeOnly=false")
 r = requests.get("https://gtaa-fl-prod.azureedge.net/api/flights/list?type=ARR&day=today&useScheduleTimeOnly=false")
-
+# path = os.getcwd()
+# print(path)
 parsed = json.loads(r.text)
 text = json.dumps(parsed, sort_keys=False, indent=4)
 f = open("arrival.txt", "w")
