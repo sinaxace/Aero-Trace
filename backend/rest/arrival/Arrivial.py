16f1e153167051693035c0d7a75333909a439c41
hunghanchen <chenhung@sheridancollege.ca>
2020-03-29 23:05:04 -0400

    commit

--- backend/rest/arrival/Arrivial.py
+++ backend/rest/arrival/Arrivial.py
@@ -1,16 +1,30 @@
+#!/usr/bin/env python
+
 import requests
 import json
 import os
+import sys
 
 
+#Get fligh API
+# yesterdayArr = requests.get("https://gtaa-fl-prod.azureedge.net/api/flights/list?type=ARR&day=yesterday&useScheduleTimeOnly=false")
+todayArr = requests.get("https://gtaa-fl-prod.azureedge.net/api/flights/list?type=ARR&day=today&useScheduleTimeOnly=false")
+#tomorrowArr = requests.get("https://gtaa-fl-prod.azureedge.net/api/flights/list?type=ARR&day=tomorrow&useScheduleTimeOnly=false")
 
-# r = requests.get("https://gtaa-fl-prod.azureedge.net/api/flights/list?type=ARR&day=today&useScheduleTimeOnly=false")
-r = requests.get("https://gtaa-fl-prod.azureedge.net/api/flights/list?type=ARR&day=today&useScheduleTimeOnly=false")
-# path = os.getcwd()
-# print(path)
-parsed = json.loads(r.text)
-text = json.dumps(parsed, sort_keys=False, indent=4)
-f = open("arrival.txt", "w")
+#Get file path
+fileDir = os.path.dirname(os.path.abspath(__file__))
+print(fileDir)
+
+parsed = json.loads(todayArr.text)
+schedule = parsed['list']
+#Get flight id
+id_list = []
+for id_ in schedule:
+    name = id_['id2']
+    id_list.append(name)
+print(id_list)    
+text = json.dumps(schedule, sort_keys=False, indent=4)
+f = open(fileDir+"\\arrival.txt", "w")
 f.write(text)
 f.close()
 
